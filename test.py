ec679592a1341f0047415c21ec4b78e16acb11ea
kimhangyu <khk201@naver.com>
2024-09-02 17:29:32 +0900

    rm cache

---
+++ test.py
@@ -0,0 +1,13 @@
+from django.shortcuts import get_object_or_404
+
+
+def post_update(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/form.html', {'form': form})
